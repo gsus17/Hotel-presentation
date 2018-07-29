7d72ec7e574f416694a86f2b97f4153e34d550f7
Suarez Jesus <suarezmolina17@gmail.com>
2018-07-29 15:49:06 -0300

    Default route config.

--- src/app/app-routes-config.ts
+++ src/app/app-routes-config.ts
@@ -1,10 +1,9 @@
 import { Routes } from '../../node_modules/@angular/router';
-
 import { AppComponent } from './app.component';
 import { HotelListComponent } from './hotel/hotel-list.component';
 
 
 export const appRoutes: Routes = [
-    { path: '', component: AppComponent },
+    { path: '', redirectTo: '/hotel-list', pathMatch: 'full' },
     { path: 'hotel-list', component: HotelListComponent }
 ];
